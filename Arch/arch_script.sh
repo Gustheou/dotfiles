55355cd8fa7021a8872750336b57d7cd8a94dc2c
Gustheou <gustavopn117@gmail.com>
2025-01-08 12:21:56 -0300

    Categorizando os pacotes

--- Arch/arch_script.sh
+++ Arch/arch_script.sh
@@ -1,14 +1,33 @@
-sudo pacman -Syu;
-sudo pacman -S dolphin kitty zsh;
-sudo pacman -S wofi waybar pavucontrol brightnessctl;
-sudo pacman -S fastfetch flatpak github-cli ttf-jetbrains-mono-nerd;
+# BEFORE EXECUTING THIS SCRIPT, I HAVE A SUGGESTION
+# (THIS STEP IS OPTIONAL!!!)
+# GO TO PACMAN CONFIG FILE AND EDIT IT WITH YOUR PREFERED EDITOR, MINE IS NEOVIM: 
+#   sudo nvim /etc/pacman.conf
+# INSIDE THE TEXT, ENABLE 'ParallelDownloads = 10'
+# ENABLE 'Color'
+# INSERT A LINE 'ILoveCandy'
 
-sudo pacman -S hyprpaper hypridle hyprlock cliphist wl-clipboard dunst man tldr bat polkit-kde-agent grim slurp ufw tlp ttf-dejavu noto-fonts noto-fonts-emoji ttf-liberation gst-libav gst-plugins-bad gst-plugins-good gst-plugins-ugly ffmpeg gstreamer hyprland xdg-desktop-portal xdg-desktop-portal-hyprland kvantum kvantum-qt5 qt5ct qt6ct nwg-look kwrite archlinux-xdg-menu keepassxc ristretto obsidian libreoffice gnome-disk-utility okular steam qbittorrent chromium galculator;
+# Update the system
+sudo pacman -Syu;
+
+# Hyprland essential packages
+sudo pacman -S wofi waybar pavucontrol brightnessctl ttf-jetbrains-mono-nerd hyprpaper hypridle hyprlock cliphist wl-clipboard hyprland xdg-desktop-portal xdg-desktop-portal-hyprland dolphin kitty;
+
+# System essential packages
+sudo pacman -S fastfetch flatpak firefox dunst man polkit-kde-agent grim slurp ufw tlp ttf-dejavu noto-fonts noto-fonts-emoji ttf-liberation gst-libav gst-plugins-bad gst-plugins-good gst-plugins-ugly ffmpeg gstreamer kvantum kvantum-qt5 qt5ct qt6ct nwg-look kwrite archlinux-xdg-menu ristretto libreoffice gnome-disk-utility okular galculator;
+
+#If you use intel, uncomment this line below:
+#sudo pacman -S intel-ucode;
+#If you use AMD, uncomment this line below:
+#sudo pacman -S amd-ucode;
 
 sudo systemctl enable --now ufw.service;
 
+# Preference packages
+sudo pacman -S keepassxc obsidian steam qbittorrent chromium yazi zsh bat tldr github-cli;
+
 flatpak install flathub com.github.d4nj1.tlpui com.discordapp.Discord md.obsidian.Obsidian org.localsend.localsend_app page.codeberg.JakobDev.jdMinecraftLauncher org.nickvision.tubeconverter flathub net.nokyan.Resources;
 
+# AUR Repository (YAY)
 cd $HOME/;
 git clone https://aur.archlinux.org/yay.git;
 cd yay;
@@ -16,9 +35,12 @@ makepkg -si;
 cd ..;
 yay -S ttf-ms-fonts visual-studio-code-bin;
 
+# Zed installer
 curl -f https://zed.dev/install.sh | sh;
-sh -c "$(curl -fsSL https://install.ohmyz.sh/)";
 
+# OhMyZShell installer
+sh -c "$(curl -fsSL https://install.ohmyz.sh/)";
+  # Plugins for ZShell
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
 #plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
@@ -29,3 +51,5 @@ email="gustavopn117@gmail.com"
 git config --global user.name $username;
 git config --global user.email $email;
 git config --list;
+
+echo "Now reboot your system and after login, type 'Hyprland'";
