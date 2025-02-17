7cdef2f7ea7e1ae3d1d5c3a2c6369b2af9e7c7ff
Gustheou <gustavopn117@gmail.com>
2025-02-17 12:31:29 -0300

    Reorganizando e adicionando pacotes.

--- Arch/arch_script.sh
+++ Arch/arch_script.sh
@@ -10,10 +10,19 @@
 sudo pacman -Syu;
 
 # Hyprland essential packages
-sudo pacman -S wofi waybar pavucontrol brightnessctl ttf-jetbrains-mono-nerd hyprpaper hypridle hyprlock cliphist wl-clipboard hyprland xdg-desktop-portal xdg-desktop-portal-hyprland dolphin kitty;
+sudo pacman -S wofi waybar pavucontrol brightnessctl hyprpaper hypridle hyprlock cliphist wl-clipboard hyprland xdg-desktop-portal xdg-desktop-portal-hyprland dolphin kitty;
 
-# System essential packages
-sudo pacman -S fastfetch flatpak firefox dunst man polkit-kde-agent grim slurp ufw tlp ttf-dejavu noto-fonts noto-fonts-emoji ttf-liberation gst-libav gst-plugins-bad gst-plugins-good gst-plugins-ugly ffmpeg gstreamer kvantum kvantum-qt5 qt5ct qt6ct nwg-look kwrite archlinux-xdg-menu ristretto libreoffice gnome-disk-utility okular galculator blueman bluez bluez-utils;
+# Fonts
+sudo pacman -S ttf-jetbrains-mono-nerd ttf-dejavu noto-fonts noto-fonts-emoji ttf-liberation;
+
+# Essentials packages (for me)
+sudo pacman -S fastfetch flatpak firefox dunst man polkit-kde-agent grim slurp ufw tlp gst-libav gst-plugins-bad gst-plugins-good gst-plugins-ugly ffmpeg gstreamer kvantum kvantum-qt5 qt5ct qt6ct nwg-look archlinux-xdg-menu ristretto okular galculator blueman bluez bluez-utils ldns yazi zsh bat tldr github-cli libreoffice gnome-disk-utility;
+
+# Preference packages
+sudo pacman -S keepassxc obsidian steam qbittorrent chromium kwrite;
+
+flatpak install flathub com.github.d4nj1.tlpui com.discordapp.Discord md.obsidian.Obsidian org.localsend.localsend_app page.codeberg.JakobDev.jdMinecraftLauncher org.nickvision.tubeconverter flathub net.nokyan.Resources;
+
 
 # Enable bluetooth service
 sudo systemctl enable bluetooth.service;
@@ -27,19 +36,14 @@ sudo systemctl start bluetooth.service;
 # Enable firewall
 sudo systemctl enable --now ufw.service;
 
-# Preference packages
-sudo pacman -S keepassxc obsidian steam qbittorrent chromium yazi zsh bat tldr github-cli;
-
-flatpak install flathub com.github.d4nj1.tlpui com.discordapp.Discord md.obsidian.Obsidian org.localsend.localsend_app page.codeberg.JakobDev.jdMinecraftLauncher org.nickvision.tubeconverter flathub net.nokyan.Resources;
-
 # AUR Repository (YAY)
 cd $HOME/;
 git clone https://aur.archlinux.org/yay.git;
 cd yay;
 makepkg -si;
 cd ..;
-yay -S ttf-ms-fonts visual-studio-code-bin;
-yay -S newsflash;
+
+yay -S ttf-ms-fonts visual-studio-code-bin newsflash;
 
 # Zed installer
 curl -f https://zed.dev/install.sh | sh;
@@ -49,13 +53,15 @@ sh -c "$(curl -fsSL https://install.ohmyz.sh/)";
   # Plugins for ZShell
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
+# MODIFY into .zshrc file
 #plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 
-username="Gustheou"
-email="gustavopn117@gmail.com"
+# INSERT your username and email and uncomment below lines.
+#username=""
+#email=""
 
-git config --global user.name $username;
-git config --global user.email $email;
-git config --list;
+#git config --global user.name $username;
+#git config --global user.email $email;
+#git config --list;
 
 echo "Now reboot your system and after login, type 'Hyprland'";
