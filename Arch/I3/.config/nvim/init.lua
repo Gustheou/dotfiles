7b3246d06ab15721504a66533d056f8d1838d707
Gustheou <gustavopn117@gmail.com>
2025-10-15 17:43:02 -0300

    i3 WM

---
+++ Arch/I3/.config/nvim/init.lua
@@ -0,0 +1,65 @@
+vim.cmd("syntax on")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set tabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set expandtab")
+vim.cmd("set cursorline")
+vim.cmd("set background=dark")
+vim.cmd("set wildmode=longest,list")
+vim.cmd("set clipboard=unnamedplus")
+vim.cmd("set mouse=a")
+vim.g.mapleader= " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {}
+local plugins = {
+  {
+    "Gustheou/solarized-osaka.nvim",
+    name = "solarized-osaka",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter", build= ":TSUpdate"
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup()
+    end,
+  }, 
+}
+
+require("lazy").setup(plugins, opts)
+require("solarized-osaka").setup()
+vim.cmd.colorscheme "solarized-osaka"
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "java", "markdown"},
+  highlight = { enable = true },
+  indent = { enable = true }
+})
---
+++ Arch/I3/arch_i3.sh
@@ -0,0 +1,51 @@
+# BEFORE EXECUTING THIS SCRIPT, I HAVE A SUGGESTION
+# (THIS STEP IS OPTIONAL!!!)
+# GO TO PACMAN CONFIG FILE AND EDIT IT WITH YOUR PREFERED EDITOR, MINE IS NEOVIM: 
+#   sudo nvim /etc/pacman.conf
+# INSIDE THE TEXT, UNCOMMENT THE LINES: 
+#   'ParallelDownloads = 10'
+#   ENABLE 'Color'
+# AND INSERT A LINE 'ILoveCandy' FOR PACMAN ANIMATION
+
+# Update the system
+sudo pacman -Syu;
+
+# i3
+sudo pacman -S i3 dmenu lightdm lightdm-gtk-greeter xdg-desktop-portal xdg-desktop-portal-gtk tumbler ffmpegthumbnailer flameshot fileroller brightnessctl thunar thunar-archive-plugin thunar-volman kitty;
+sudo pacman -S xed firefox flatpak dunst man ufw gst-libav gst-plugins-bad gst-plugins-good gst-plugins-ugly ffmpeg gstreamer archlinux-xdg-menu feh okular galculator blueman bluez bluez-utils ldns yazi github-cli libreoffice gnome-disk-utility mpv zip unzip p7zip unrar tar gzip keepassxc obsidian steam qbittorrent chromium ncdu discord;
+
+flatpak install flathub com.github.d4nj1.tlpui org.localsend.localsend_app page.codeberg.JakobDev.jdMinecraftLauncher org.nickvision.tubeconverter info.febvre.Komikku net.ankiweb.Anki
+
+
+# Enable bluetooth service
+sudo systemctl enable bluetooth.service;
+#sudo systemctl start bluetooth.service;
+
+#If you use intel, uncomment this line below:
+sudo pacman -S intel-ucode;
+#If you use AMD, uncomment this line below:
+#sudo pacman -S amd-ucode;
+
+# Enable firewall
+sudo systemctl enable --now ufw.service;
+
+# AUR Repository (YAY)
+cd $HOME/;
+git clone https://aur.archlinux.org/yay.git;
+cd yay;
+makepkg -si;
+cd ..;
+
+yay -S ttf-ms-fonts visual-studio-code-bin wordgrinder android-studio min-browser-bin;
+
+## ZSH-Section
+#sudo pacman -S zsh
+# OhMyZShell installer
+#sh -c "$(curl -fsSL https://install.ohmyz.sh/)";
+  # Plugins for ZShell
+#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
+# MODIFY into .zshrc file
+#plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+
+echo "Now reboot your system and after login, type 'Hyprland'";
