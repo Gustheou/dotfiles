f99ab39e9ae36a912de9f9e35cea2d61432ddfa6
Gustheou <gustavopn117@gmail.com>
2025-10-24 12:45:34 -0300

    I3 update

--- Arch/I3/arch_i3.sh
+++ Arch/I3/arch_i3.sh
@@ -11,23 +11,16 @@
 sudo pacman -Syu;
 
 # i3
-sudo pacman -S i3 dmenu lightdm lightdm-gtk-greeter xdg-desktop-portal xdg-desktop-portal-gtk tumbler ffmpegthumbnailer flameshot fileroller brightnessctl thunar thunar-archive-plugin thunar-volman kitty;
-sudo pacman -S xed firefox flatpak dunst man ufw gst-libav gst-plugins-bad gst-plugins-good gst-plugins-ugly ffmpeg gstreamer archlinux-xdg-menu feh okular galculator blueman bluez bluez-utils ldns yazi github-cli libreoffice gnome-disk-utility mpv zip unzip p7zip unrar tar gzip keepassxc obsidian steam qbittorrent chromium ncdu discord;
+sudo pacman -S i3 dmenu lightdm lightdm-gtk-greeter xdg-desktop-portal xdg-desktop-portal-gtk tumbler ffmpegthumbnailer flameshot brightnessctl kitty pcmanfm xarchiver;
+#thunar thunar-archive-plugin thunar-volman fileroller
+sudo pacman -S xed firefox flatpak dunst man ufw gst-libav gst-plugins-bad gst-plugins-good gst-plugins-ugly ffmpeg gstreamer archlinux-xdg-menu feh okular galculator blueman bluez bluez-utils network-manager-applet ldns yazi github-cli libreoffice gnome-disk-utility mpv zip unzip p7zip unrar tar gzip keepassxc obsidian steam qbittorrent chromium ncdu discord;
 
-flatpak install flathub com.github.d4nj1.tlpui org.localsend.localsend_app page.codeberg.JakobDev.jdMinecraftLauncher org.nickvision.tubeconverter info.febvre.Komikku net.ankiweb.Anki
-
-
-# Enable bluetooth service
-sudo systemctl enable bluetooth.service;
-#sudo systemctl start bluetooth.service;
+flatpak install flathub org.localsend.localsend_app page.codeberg.JakobDev.jdMinecraftLauncher org.nickvision.tubeconverter info.febvre.Komikku net.ankiweb.Anki
 
 #If you use intel, uncomment this line below:
 sudo pacman -S intel-ucode;
 #If you use AMD, uncomment this line below:
 #sudo pacman -S amd-ucode;
-
-# Enable firewall
-sudo systemctl enable --now ufw.service;
 
 # AUR Repository (YAY)
 cd $HOME/;
@@ -36,7 +29,7 @@ cd yay;
 makepkg -si;
 cd ..;
 
-yay -S ttf-ms-fonts visual-studio-code-bin wordgrinder android-studio min-browser-bin;
+yay -S st ttf-ms-fonts visual-studio-code-bin android-studio min-browser-bin;
 
 ## ZSH-Section
 #sudo pacman -S zsh
@@ -48,4 +41,14 @@ yay -S ttf-ms-fonts visual-studio-code-bin wordgrinder android-studio min-browse
 # MODIFY into .zshrc file
 #plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 
-echo "Now reboot your system and after login, type 'Hyprland'";
+# Enable login display manager
+sudo systemctl enable lightdm;
+
+# Enable bluetooth service
+sudo systemctl enable bluetooth.service;
+#sudo systemctl start bluetooth.service;
+
+# Enable firewall
+sudo systemctl enable --now ufw.service;
+
+echo "Now reboot your system!!!";
