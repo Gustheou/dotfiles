1468e2a8db7ae97a98ed3723efd7d6e3ad2a7d49
Gustheou <gustavopn117@gmail.com>
2026-08-17 08:10:13 -0300

    refactoring

--- Arch/i3/arch_i3.sh
+++ Arch/i3/arch_i3.sh
@@ -10,42 +10,26 @@
 # Update the system
 sudo pacman -Syu;
 
-# i3
-sudo pacman -S i3 dmenu lightdm lightdm-gtk-greeter xdg-desktop-portal xdg-desktop-portal-gtk tumbler ffmpegthumbnailer flameshot brightnessctl kitty pcmanfm xarchiver gvfs;
-#thunar thunar-archive-plugin thunar-volman fileroller
-sudo pacman -S xed firefox flatpak dunst man ufw gst-libav gst-plugins-bad gst-plugins-good gst-plugins-ugly ffmpeg gstreamer archlinux-xdg-menu feh okular galculator blueman bluez bluez-utils network-manager-applet ldns yazi github-cli libreoffice gnome-disk-utility mpv zip unzip p7zip unrar tar gzip keepassxc obsidian steam qbittorrent chromium ncdu discord;
+sudo pacman -S ttf-dejavu;
 
-flatpak install flathub org.localsend.localsend_app page.codeberg.JakobDev.jdMinecraftLauncher org.nickvision.tubeconverter info.febvre.Komikku net.ankiweb.Anki
+# i3
+sudo pacman -S i3 dmenu lightdm lightdm-gtk-greeter xdg-desktop-portal xdg-desktop-portal-gtk tumbler ffmpegthumbnailer flameshot brightnessctl kitty pcmanfm xarchiver xreader xed gvfs trash-cli smartmontools power-profiles-daemon pavucontrol dunst man-db feh borg xclip xdg-utils;
+
+sudo pacman -S firefox flatpak ufw gst-libav gst-plugins-bad gst-plugins-good gst-plugins-ugly ffmpeg gstreamer galculator blueman bluez bluez-utils network-manager-applet networkmanager ldns github-cli libreoffice gnome-disk-utility mpv zip unzip unrar tar gzip keepassxc obsidian steam steam-devices ncdu discord geany;
+
+flatpak install flathub org.localsend.localsend_app page.codeberg.JakobDev.jdMinecraftLauncher org.nickvision.tubeconverter net.ankiweb.Anki org.taisei_project.Taisei
 
 #If you use intel, uncomment this line below:
 sudo pacman -S intel-ucode;
 #If you use AMD, uncomment this line below:
 #sudo pacman -S amd-ucode;
 
-# AUR Repository (YAY)
-cd $HOME/;
-git clone https://aur.archlinux.org/yay.git;
-cd yay;
-makepkg -si;
-cd ..;
-
-yay -S st ttf-ms-fonts visual-studio-code-bin android-studio min-browser-bin;
-
-## ZSH-Section
-#sudo pacman -S zsh
-# OhMyZShell installer
-#sh -c "$(curl -fsSL https://install.ohmyz.sh/)";
-  # Plugins for ZShell
-#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
-#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
-# MODIFY into .zshrc file
-#plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 
 # Enable login display manager
 sudo systemctl enable lightdm;
 
 # Enable bluetooth service
-sudo systemctl enable bluetooth.service;
+#sudo systemctl enable bluetooth.service;
 #sudo systemctl start bluetooth.service;
 
 # Enable firewall
